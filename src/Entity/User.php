13cea80d950f0e8debe0c36c3f4a4125d3feedf5
Hakan Akca <hakan.akca@supinternet.fr>
2017-12-21 11:59:59 +0100

    Add register controller and template

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,3 +1,5 @@
+<?php
+
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
@@ -22,41 +24,9 @@ class User implements UserInterface
     /**
      * @ORM\Column(type="string", length=255, unique=true)
      * @Assert\NotBlank()
-     * @Assert\Email()
-     */
-    private $email;
-
-    /**
-     * @ORM\Column(type="string", length=255, unique=true)
-     * @Assert\NotBlank()
      */
     private $username;
 
-    /**
-     * @Assert\NotBlank()
-     * @Assert\Length(max=4096)
-     */
-    private $plainPassword;
-
-    /**
-     * The below length depends on the "algorithm" you use for encoding
-     * the password, but this works well with bcrypt.
-     *
-     * @ORM\Column(type="string", length=64)
-     */
-    private $password;
-
-    // other properties and methods
-
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    }
 
     public function getUsername()
     {
@@ -66,33 +36,6 @@ class User implements UserInterface
     public function setUsername($username)
     {
         $this->username = $username;
-    }
-
-    public function getPlainPassword()
-    {
-        return $this->plainPassword;
-    }
-
-    public function setPlainPassword($password)
-    {
-        $this->plainPassword = $password;
-    }
-
-    public function getPassword()
-    {
-        return $this->password;
-    }
-
-    public function setPassword($password)
-    {
-        $this->password = $password;
-    }
-
-    public function getSalt()
-    {
-        // The bcrypt algorithm doesn't require a separate salt.
-        // You *may* need a real salt if you choose a different encoder.
-        return null;
     }
 
     // other methods, including security methods like getRoles()
